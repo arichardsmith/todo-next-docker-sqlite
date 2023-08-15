6da029e1e959440f72308053381d3529fa8a7cc9
Richard Smith <richardmcsmith@gmail.com>
2023-08-15 17:48:36 +0900

    Remove old config file

--- lib/data/config.ts
+++
@@ -1,6 +0,0 @@
-import { join } from "node:path";
-import { packageDirectorySync } from "pkg-dir";
-
-export const DB_ROOT = join(packageDirectorySync() ?? "", "./database/");
-
-export const DB_PATH = process.env.DB_PATH ?? join(DB_ROOT, "data.sqlite");
